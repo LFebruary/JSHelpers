0eeac467946b3a490b633d9b0443d993f2cb29aa
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-03-01 11:06:53 +0200

    Create class_observer.js

---
+++ class_observer.js
@@ -0,0 +1,92 @@
+/**
+ * Watches for changes in a specified class on a target node and triggers callbacks accordingly.
+ */
+class ClassObserver {
+    /**
+     * Constructs a new ClassObserver.
+     * @param {Node} target  - The target node to observe.
+     * @param {string} classToWatch - The class to watch for changes.
+     * @param {Function} onClassAdded  - The callback function to execute when the class is added.
+     * @param {Function} onClassRemoved  - The callback function to execute when the class is removed.
+     */
+    constructor(target, classToWatch, onClassAdded, onClassRemoved) {
+        /**
+         * The target node to observe for class changes.
+         * @type {Node}
+         */
+        this.target = target;
+
+        /**
+         * The class to watch for changes.
+         * @type {string}
+         */
+        this.classToWatch = classToWatch;
+
+        /**
+         * The callback function to execute when the class is added.
+         * @type {Function}
+         */
+        this.onClassAdded = onClassAdded;
+
+        /**
+         * The callback function to execute when the class is removed.
+         * @type {Function}
+         */
+        this.onClassRemoved = onClassRemoved;
+
+        /**
+         * The MutationObserver instance used to observe changes.
+         * @type {MutationObserver|null}
+         */
+        this.observer = null;
+
+        /**
+         * The last state of the target node's class.
+         * @type {boolean}
+         */
+        this.lastClassState = targetNode.classList.contains(this.classToWatch);
+    }
+
+    /**
+     * Initializes the observer.
+     */
+    init() {
+        this.observer = new MutationObserver(this.mutationCallback)
+        this.observe()
+    }
+
+    /**
+     * Starts observing changes.
+     */
+    observe() {
+        this.observer.observe(this.targetNode, { attributes: true })
+    }
+
+    /**
+     * Disconnects the observer.
+     */
+    disconnect() {
+        this.observer.disconnect()
+    }
+
+    /**
+     * Mutation callback function.
+     * @param {MutationRecord[]} mutationsList - The list of mutations.
+     */
+    mutationCallback = mutationsList => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                const currentClassState = mutation.target.classList.contains(this.classToWatch)
+                if (this.lastClassState !== currentClassState) {
+                    this.lastClassState = currentClassState
+                    if (currentClassState) {
+                        this.onClassAdded()
+                    }
+                    else {
+                        this.onClassRemoved()
+                    }
+                }
+            }
+        }
+    }
+}
