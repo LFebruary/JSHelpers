f39914b1820c5b544b220d152522e15e5b67ee8b
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-03-13 23:22:41 +0200

    Lets do this

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 /*
- * JSHelpers v1.0.0 https://github.com/LFebruary/JSHelpers
+ * JSHelpers v0.0.8 https://github.com/LFebruary/JSHelpers
  * (c) 2024 LFebruary - Released under the MIT License (https://github.com/LFebruary/JSHelpers/blob/master/LICENSE)
  */
 import { ClassObserver } from "./class_observer";
