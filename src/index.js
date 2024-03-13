6dd38f4ed462aff2ba869ce6ea4eef01081bcde3
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-03-13 23:29:55 +0200

    Version bump to v$npm_package_version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 /*
- * JSHelpers v0.0.12 https://github.com/LFebruary/JSHelpers
+ * JSHelpers v0.0.13 https://github.com/LFebruary/JSHelpers
  * (c) 2024 LFebruary - Released under the MIT License (https://github.com/LFebruary/JSHelpers/blob/master/LICENSE)
  */
 import { ClassObserver } from "./class_observer";
