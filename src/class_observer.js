169274803975f323cb53535711612512574d6961
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-03-17 23:57:00 +0200

    Version bump to v0.0.17

--- src/class_observer.js
+++ src/class_observer.js
@@ -1,5 +1,5 @@
 /*
- * JSHelpers v0.0.16 https://github.com/LFebruary/JSHelpers
+ * JSHelpers v0.0.17 https://github.com/LFebruary/JSHelpers
  * (c) 2024 LFebruary - Released under the MIT License (https://github.com/LFebruary/JSHelpers/blob/master/LICENSE)
  */
 
@@ -8,6 +8,36 @@
  */
 class ClassObserver {
   /**
+   * The target node to observe for class changes.
+   * @type {Node}
+   */
+  #target;
+
+  /**
+  * The class to watch for changes.
+  * @type {string}
+  */
+  #classToWatch;
+
+  /**
+   * The callback function to execute when the class is added.
+   * @type {Function}
+   */
+  #onClassAdded;
+
+  /**
+   * The callback function to execute when the class is removed.
+   * @type {Function}
+   */
+  #onClassRemoved;
+
+  /**
+   * The MutationObserver instance used to observe changes.
+   * @type {MutationObserver|null}
+   */
+  #observer;
+
+  /**
    * Constructs a new ClassObserver.
    * @param {Node} target  - The target node to observe.
    * @param {string} classToWatch - The class to watch for changes.
@@ -15,58 +45,33 @@ class ClassObserver {
    * @param {Function} onClassRemoved  - The callback function to execute when the class is removed.
    */
   constructor(target, classToWatch, onClassAdded, onClassRemoved) {
-    /**
-     * The target node to observe for class changes.
-     * @type {Node}
-     */
-    this.target = target;
-
-    /**
-     * The class to watch for changes.
-     * @type {string}
-     */
-    this.classToWatch = classToWatch;
-
-    /**
-     * The callback function to execute when the class is added.
-     * @type {Function}
-     */
-    this.onClassAdded = onClassAdded;
-
-    /**
-     * The callback function to execute when the class is removed.
-     * @type {Function}
-     */
-    this.onClassRemoved = onClassRemoved;
-
-    /**
-     * The MutationObserver instance used to observe changes.
-     * @type {MutationObserver|null}
-     */
-    this.observer = null;
+    this.#target = target;
+    this.#classToWatch = classToWatch;
+    this.#onClassAdded = onClassAdded;
+    this.#onClassRemoved = onClassRemoved;
+    this.#observer = null;
   }
 
   /**
    * Initializes the observer.
    */
   init() {
-    this.observer = new MutationObserver(this.mutationCallback);
-    this.observe();
-    Object.freeze(this);
+    this.#observer = new MutationObserver(this.mutationCallback);
+    this.#observe();
   }
 
   /**
    * Starts observing changes.
    */
-  observe() {
-    this.observer.observe(this.target, { attributes: true });
+  #observe() {
+    this.#observer.observe(this.#target, { attributes: true });
   }
 
   /**
    * Disconnects the observer.
    */
   disconnect() {
-    this.observer.disconnect();
+    this.#observer.disconnect();
   }
 
   /**
@@ -80,14 +85,14 @@ class ClassObserver {
         mutation.attributeName === "class"
       ) {
         const currentClassState = mutation.target.classList.contains(
-          this.classToWatch,
+          this.#classToWatch,
         );
         if (this.lastClassState !== currentClassState) {
           this.lastClassState = currentClassState;
           if (currentClassState) {
-            this.onClassAdded();
+            this.#onClassAdded();
           } else {
-            this.onClassRemoved();
+            this.#onClassRemoved();
           }
         }
       }
